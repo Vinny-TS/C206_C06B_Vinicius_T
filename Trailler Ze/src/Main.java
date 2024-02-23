31ced1e6dc5185aaf9c121a04da1f1774ed1035b
Vinícius Silva <81161923+Vinny-TS@users.noreply.github.com>
2024-02-22 22:30:03 -0300

    Upando Trailler Ze

---
+++ Trailler Ze/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        float lanche1 = 10f;
+        float lanche2 = 4f;
+        float lanche3 = 2f;
+
+        float totalLanche = lanche1 + lanche2 + lanche3;
+
+        float media = totalLanche/3;
+
+        System.out.println(totalLanche);
+        System.out.println(media);
+    }
+}
